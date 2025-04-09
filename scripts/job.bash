56ebf15cf030ae6c2d9f446d88fff1ac4eaf6741
michalspano <spano@michalspano.com>
2025-04-09 22:59:10 +0200

    #4 Update job.bash with nvidia-smi profiling

--- scripts/job.bash
+++ scripts/job.bash
@@ -1,42 +1,25 @@
 #!/usr/bin/env bash
 #SBATCH -A NAISS2025-22-104 -p alvis
-#SBATCH --gpus-per-node=A100:2
+#SBATCH --gpus-per-node=A40:1
 #SBATCH -t 0-01:00:00
-
-# ---------- WIP ---------------- #
 
 ml purge # good practice
 
-datetime=$(date)
+file="./logs/nvidia/job-new.log"
+rm $file
+PROBE_INTERVAL_MS=1000
 
-file="./logs/nvidia/job2.log"
-echo "" > $file
-
-# === MONITOR FUNCTION ===
-# https://nvidia.custhelp.com/app/answers/detail/a_id/3751/~/useful-nvidia-smi-queries
-monitor() {
-    # Use --loop flag
-    # nvidia-smi --query-gpu=timestamp,name,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv --loop 1 >> $file
-    # TODO other script here to ensure that the csv entries collide to a one BIG CSV
-    # GPU_usage | timestamp
-    # x         | y
-    # .         | .
-    i=1
-    while true; do
-    # What I used before
-    # nvidia-smi --query-compute-apps=pid,gpu_uuid,gpu_name,used_gpu_memory --format=csv,noheader
-       echo "=*=" >> $file
-       sleep 1 # interval to sleep
-       ((i++))
-    done
+__monitor() {
+    echo $QUERY # CSV header
+    QUERY="timestamp,gpu_uuid,utilization.gpu,utilization.memory,memory.used,temperature.gpu"
+    nvidia-smi --query-gpu=$QUERY --format=csv,noheader --loop-ms=$PROBE_INTERVAL_MS >> $file
 }
 
-monitor &
+__monitor &
 MONITOR_PID=$!
 
 PROJ_PATH="/mimer/NOBACKUP/groups/naiss2025-22-104/REST/REST-at"
 
-# === ARGUMENTS ===
 SESSION_NAME=$1
 MODEL=$2
 DATA=$3
