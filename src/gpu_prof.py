563e0e76db5068e1c4bd3d416a86428b976e0637
michalspano <michal.spano03@gmail.com>
2025-04-30 19:19:56 +0200

    #4 Update docs for `src/gpu_prof.py`

--- src/gpu_prof.py
+++ src/gpu_prof.py
@@ -1,26 +1,33 @@
-# This is just a simple script. Explain it in more detail.
+"""
+A simple script that uses the GPUProfiler and inserts a profile Object onto a
+result Object from `send_data.py`.
+"""
 
-import json
 from sys import argv
+from json import load, dump
 
 from .core.gpu_profiler import GPUProfiler, ProfileResponse
 
 if __name__ == '__main__':
-    if (len(argv)) < 3:
-        exit(1)
-
-    profile = argv[1]
-    logfile = argv[2]
-
-    gpu: GPUProfiler = GPUProfiler(profile, 0.25)
+    if (len(argv)) < 3: exit(1)
+    
+    profile = argv[1] # Which profile to inject
+    logfile = argv[2] # The `res.json` file to inject the profile to
+    
+    # Initiate a GPUProfiler instance, compute table
+    gpu: GPUProfiler     = GPUProfiler(profile, 0.25)
     res: ProfileResponse = gpu.compute() 
 
-    if res is None: exit(1)
+    if res is None: exit(1) # logged internally
 
     with open(logfile, 'r+') as file:
-        ctx = json.load(file)
-        ctx['data'].update(res)
+        # Load current state of the file and update 'data' section
+        ctx = load(file)
+        try:
+            ctx['data'].update(res)
+        except Exception:
+            print(f'Cannot update: {logfile}: {Exception}')
 
-        file.seek(0)
-        json.dump(ctx, file, indent=4)
-        file.truncate()
+        file.seek(0)              # Navigate to the beginning
+        dump(ctx, file, indent=4) # Dump updated JSON 
+        file.truncate()           # Resize byte number
