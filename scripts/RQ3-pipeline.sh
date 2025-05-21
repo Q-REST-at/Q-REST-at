3da98c76984cb5007e9d155a8669a3b631b92050
michalspano <michal.spano03@gmail.com>
2025-05-22 00:24:57 +0200

    #39 Add print for easy of use and delay

--- scripts/RQ3-pipeline.sh
+++ scripts/RQ3-pipeline.sh
@@ -12,11 +12,15 @@ mkdir -p "./logs"
 
 for dataset in "${datasets[@]}"; do
     for sample_size in "${sample_sizes[@]}"; do
+
+        echo "dataset: $dataset, sample: $sample_size"
+
         session="RQ3_${dataset}_${sample_size}"
         new_dataset="RQ3:${dataset}:${sample_size}"
         REST_AT_FLAGS="$session $model $new_dataset gptq.sif GPTQ"
 
         sbatch ./scripts/job.bash $REST_AT_FLAGS $ITER_PER_SESSION
+        sleep 30
     done
 done
 
