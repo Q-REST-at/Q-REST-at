66638eb8ee555defaf0ca795a421c08f24a2e808
Mariia Zabolotnia <79398381+marichkazb@users.noreply.github.com>
2025-05-24 10:36:02 +0200

    fix container declaration

--- scripts/RQ3-pipeline.sh
+++ scripts/RQ3-pipeline.sh
@@ -4,7 +4,7 @@ ml purge
 datasets=("AMINA" "Mozilla")
 sample_sizes=(5 10 15 25 50 75 100)
 model="MIS"
-quant="GPTQ"
+quant="NONE"
 
 ITER_PER_SESSION=10
 
@@ -19,8 +19,17 @@ for dataset in "${datasets[@]}"; do
 
         session="RQ3_${dataset}_${sample_size}"
         new_dataset="RQ3-${dataset}-${sample_size}"
+        if [[ "$quant" == "AWQ" ]]; then
+            container="awq.sif"
+        elif [[ "$quant" == "GPTQ" ]]; then
+            container="gptq.sif"
+        elif [[ "$quant" == "AQLM" ]]; then
+            container="aqlm.sif"
+        else
+            container="container.sif" # default; no quantization
+        fi
 
-        REST_AT_FLAGS=("$session" "$model" "$new_dataset" "${quant,,}" "$quant" "$ITER_PER_SESSION")
+        REST_AT_FLAGS=("$session" "$model" "$new_dataset" "$container" "$quant" "$ITER_PER_SESSION")
         sbatch -o logs/"$session".log ./scripts/job.bash "${REST_AT_FLAGS[@]}"
 
         sleep 30
