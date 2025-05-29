d8bc358548863244c71ced6b4929b28c2656f4e9
michalspano <michal.spano03@gmail.com>
2025-05-30 01:54:18 +0200

    #43, #23, #22 Update scripts pre-submission

--- src/summary_table_v2.py
+++ src/summary_table_v2.py
@@ -1,4 +1,3 @@
-# TODO: remap metric names properly
 import re
 import pandas as pd
 
@@ -31,19 +30,15 @@ def parse_uncertainty(value: str) -> tuple[float, float]:
 # for n_col in numeric_cols:
 #     df[n_col] = df[n_col].apply(format_latex_mathmode)
 
-highlight_color: str = "9fd6b5" # that's what we decided on ultimately
 
-REMAP_DATASET: dict[str, str | None] = {
-    "AMINA":   None,
-    "BTHS":    None,
-    "HW":      "Health Watcher",
-    "MOZILLA": "Mozilla"
-}
-
-def find_dataset_label(x: str) -> str:
-    l: str | None = REMAP_DATASET[x.upper()]
-    return x if l is None else l
-
+highlight_colors = [
+    "fff2ac",  # pale yellow
+    "c2f0c2",  # light green
+    "add8e6",  # light blue
+    "f7c6c7",  # light pink
+    "e0bbff",  # light purple
+    "ffd9b3",  # light orange
+]
 
 df['dataset'] = df['Treatment'].apply(lambda x: x.split("_")[2])
 df['Treatment'] = df['Treatment'].apply(lambda x: x.split("_")[1])
@@ -54,7 +49,7 @@ tables: list[str] = []
 for dataset, df in group_by_dataset:
     del df['dataset']
 
-    for n_col in numeric_cols:
+    for i, n_col in enumerate(numeric_cols):
         df[f'{n_col}_tuple'] = df[n_col].apply(parse_uncertainty)
 
         if n_col in min_cols:
@@ -63,7 +58,7 @@ for dataset, df in group_by_dataset:
             best = max(df[f'{n_col}_tuple'], key=lambda x : (x[0], -x[1]))
         
         df[f'{n_col}_str'] = df[f'{n_col}_tuple'].apply(
-            lambda x: f"\\colorbox{{hl_color_green}}{{{format_latex_mathmode(x)}}}" if x == best else format_latex_mathmode(x)
+            lambda x: f"\\colorbox{{color{i+1}}}{{{format_latex_mathmode(x)}}}" if x == best else format_latex_mathmode(x)
         )
 
     df = df[['Treatment'] + [col for col in df.columns if col.endswith('_str')]]
@@ -73,20 +68,24 @@ for dataset, df in group_by_dataset:
     latex_table = df.to_latex(
         index=False,
         escape=False,
-        column_format="p{1cm} p{2.2cm} p{1.5cm} p{1.5cm} p{1.5cm} p{2cm} p{2.8cm}"
+        column_format='l'*len(df.columns),
+        caption=str(dataset)
     )
 
     table = (
         "\\begin{subtable}{\\textwidth}\n"
         "\\centering\n"
-        f"\\caption*{{{find_dataset_label(dataset)} dataset}}\n"
         + latex_table +
         "\\end{subtable}\n"
     )
     tables.append(table)
 
+colors = (
+    f"\\definecolor{{color{i+1}}}{{HTML}}{{{color}}}\n" for i, color in enumerate(highlight_colors)
+)
+
 final_table: str = (
-    f"\\definecolor{{hl_color_green}}{{HTML}}{{{highlight_color}}}\n\n"
+    "".join(colors) + "\n" +
     "\\begin{table*}[ht]\n"
     "\\centering\n"
     + "\n".join(table.replace("\\begin{table}", "").replace("\\end{table}", "") for table in tables) +
